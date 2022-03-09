dfc7b93ce9b1be88a7e77e4b23b5adacfd28d4eb
Kamil Kwiecinski <kkwiecinski18@gmail.com>
2022-03-09 19:40:02 +0100

    6.2 Poprawa

--- kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -1,23 +1,12 @@
 package com.kodilla.testing.calculator;
 
 public class Calculator {
-    int firstNumber;
-    int secondNumber;
-
-    public Calculator(int firstNumber, int secondNumber){
-        this.firstNumber = firstNumber;
-        this.secondNumber = secondNumber;
-    }
 
     public int add(int a, int b){
-        int result;
-        result = a + b;
-        return result;
+        return a + b;
     }
 
     public int subtract(int a, int b){
-        int result;
-        result = a - b;
-        return result;
+        return a - b;
     }
 }
