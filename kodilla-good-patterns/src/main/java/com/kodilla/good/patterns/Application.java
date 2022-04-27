9a73a1dcbb3ec218b33ddb8707cba8eee2699a10
Kamil Kwiecinski <kkwiecinski18@gmail.com>
2022-04-27 21:52:01 +0200

    first commit

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/Application.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/Application.java
@@ -1,2 +1,19 @@
-package com.kodilla.good.patterns;public class Application {
+package com.kodilla.good.patterns;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Application {
+    public static void main(String[] args) {
+        MovieStore movieStore = new MovieStore();
+        Map<String, List<String>> map =  movieStore.getMovies();
+
+        String movies = map.entrySet()
+                .stream()
+                .flatMap(n -> n.getValue().stream())
+                .collect(Collectors.joining(" ! "));
+        System.out.print(movies);
+
+    }
 }
